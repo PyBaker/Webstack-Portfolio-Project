e7e6eac3c08397809e0f29e899f8cdced94dfbb2
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-10-19 22:06:44 +0200

    refactor: hotfix

--- web/static/styles/main.js
+++ web/static/styles/main.js
@@ -1,1 +1,10 @@
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+var countDownDate = new Date("Nov 5, 2022 15:37:25").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+}
+}, 1000);
