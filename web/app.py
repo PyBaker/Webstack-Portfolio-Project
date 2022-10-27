a93976ba032a21fc282ea8980794739f2446775d
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-27 20:20:31 +0300

    feat(login): add route for the login page

--- web/app.py
+++ web/app.py
@@ -1,11 +1,13 @@
 """
 Defines routes of the project
 """
+import secrets
 from processpass import encryptpass 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from tables import RegisteredVoters, Post, Aspirants
-from flask import Flask, request, render_template, redirect
+from tables import RegisteredVoters, Post, Aspirants, Voters, Admin
+from flask import Flask, request, render_template, redirect, make_response, jsonify
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 # connects to database
 username = 'rod'
@@ -15,7 +17,19 @@ engine = create_engine(str1)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
+app.secret_key = secrets.token_bytes()
 
+# enable user login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# return current user
+@login_manager.user_loader
+def load_user(user_id):
+    """returns the current logged in user id"""
+
+    return session.query(RegisteredVoters).filter(RegisteredVoters.id == int(user_id)).first()
 
 # allow both GET and POST requests
 @app.route('/form-example', methods=['GET', 'POST'])
@@ -52,25 +66,59 @@ def login():
     if request.method == 'POST':
         login_password = request.form.get('password')
         login_userid = request.form.get('user_id')
+        #print(type(login_password))
+        #return f'{login_password}'
+        
+        # check if admin -- take to admin panel
+        admin = session.query(Admin).filter(Admin.id_no == int(login_userid)).first()
+        if admin:
+            if admin.Password.decode('ascii') != login_password:
+                return 'Wrong password!'
+            login_user(admin)
+            return redirect('/admin_panel')
+        else:
+            # get user
+            user = load_user(int(login_userid))
+            #  Check if user in Database and confirm login details
+            if not user:
+                return 'Not a registered voter!'
+            if user.Password.decode('ascii') != login_password:
+                return 'Wrong password!'
 
-        # Check if admin
-        #
-        # Take to admin panel
-        #
-        #  Check if user in Database
-        #
-        # Take to voting_screen if casted votes are less that 6
- 
+            # Take to voting_screen if casted votes are less that 6
+            voter = session.query(Voters).filter(Voters.id_no == int(login_userid)).first()
+            if (not voter) or (voter.status == "NV"):
+                login_user(user)
+                return redirect('/voting_screen')
+            else:
+                # take to results page??
+                return redirect('/results_page')
     return render_template('login_page.html')
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    """Logs out the current user"""
+    logout_user()
+    return 'You are now logged out!'
+
+
 @app.route('/register-users', methods=['GET', 'POST'])
+@login_required
 def register_user():
     """
     Handles registration of users
     """
+
+    # make this page only available to admins -- do the same for the other admin page
+    if request.method == "GET":
+        admin_id = current_user.id
+        admin = session.query(Admin).filter(Admin.id_no == int(admin_id)).first()
+        if not admin:
+            return 'Admin Page! Please visit the log in page to log in'
     if request.method == "POST":
-        idno = request.form.get("id_no")
+        id_no = request.form.get("id_no")
         firstname = request.form.get("first_name").upper()
         middlename = request.form.get("middle_name").upper()
         lastname = request.form.get("last_name").upper()
@@ -80,7 +128,7 @@ def register_user():
 
         # Writing to the database
         User = RegisteredVoters()
-        User.id_no = idno
+        User.id = int(id_no)
         User.First_Name = firstname
         User.Middle_Name = middlename
         User.Last_Name = lastname
@@ -91,18 +139,22 @@ def register_user():
         # Comitting User to database
         session.add(User)
         session.commit()
-        return "You have Successfully Registred"
+        # display voter information i.e name, reg_no with this message below
+        return "You have Successfully Registred the voter"
 
     return render_template('registration_page.html')
 
 
 @app.route('/register-aspirants', methods=['GET', 'POST'])
+@login_required
 def register_aspirants():
     """
     Handles registration of Aspirants
     """
+
+    # check if aspirant is a registered voter first
     if request.method == "POST":
-        idno = request.form.get("idno")
+        id_no = request.form.get("id_no")
         postname = request.form.get("post_name").upper()
         firstname = request.form.get("first_name").upper()
         middlename = request.form.get("middle_name").upper()
@@ -112,8 +164,9 @@ def register_aspirants():
         email = request.form.get("email")
 
         # Writing to the database
+        # get aspirant from registered voters -- check if exists
         Aspirant = Aspirants()
-        Aspirant.id_no = idno
+        Aspirant.id_no = int(id_no)
         Aspirant.Post_Name = postname
         Aspirant.First_Name = firstname
         Aspirant.Middle_Name = middlename
@@ -131,6 +184,7 @@ def register_aspirants():
 
 
 @app.route('/register-post', methods=['GET', 'POST'])
+@login_required
 def register_post():
     """
     Handles registration of Posts
@@ -150,6 +204,7 @@ def register_post():
 
 
 @app.route('/select_asp', methods=['POST', 'GET'])
+@login_required
 def select_asp():
     """
     Takes you to aspirant voting page
@@ -177,6 +232,7 @@ def sent_vote():
 
 
 @app.route('/admin_panel')
+@login_required
 def admin_panel():
     """
     Takes you to admin panel
@@ -185,11 +241,17 @@ def admin_panel():
 
 
 @app.route('/voting_screen')
+@login_required
 def voting_screen():
     """
     Takes you to voting screen
     """
-    return render_template('voting_screen.html')
+    return render_template('voting_screen.html',
+        user_f_name=current_user.First_Name,
+        user_l_name=current_user.Last_Name,
+        id=current_user.id,
+        reg_no=current_user.reg_no
+        )
 
 
 @app.route('/results_page')
--- web/tables.py
+++ web/tables.py
@@ -1,10 +1,42 @@
+#!/usr/bin/env python3
+"""Define tables for the database"""
 import enum
+from xmlrpc.client import Boolean
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, LargeBinary, String
+from sqlalchemy import create_engine, Column, Integer, LargeBinary, String, Boolean
 from sqlalchemy import DateTime, Enum, ForeignKey
 from sqlalchemy.sql import func
+import uuid
+from flask_login import UserMixin
 
+username = "rod"
+password = "r"
+str1 = f'mysql://{username}:{password}@localhost:3306/VOTEAPP'  # Holds database info
+engine = create_engine(str1)
 Base = declarative_base()
+
+
+class BaseModel():
+    """Defines the to_dict method that returns a dictionary of each
+    class's attributes to enable conversion to json objects on the APIs"""
+
+    def to_dict(self):
+        """returns dictionary of the instance's attributes"""
+
+        my_dict = self.__dict__.copy()
+
+        # remove sensitive information from my_dict
+        if "Password" in my_dict:
+            my_dict.pop('Password')
+        if "Email" in my_dict:
+            my_dict.pop("Email")
+        if "id_no" in my_dict:
+            my_dict.pop("id_no")
+
+        my_dict.pop('_sa_instance_state')
+
+        return my_dict
+
 
 
 class myEnum(enum.Enum):
@@ -16,12 +48,13 @@ class myEnum(enum.Enum):
     NV = "Not Voted"
 
 
-class RegisteredVoters(Base):
+class RegisteredVoters(Base, UserMixin, BaseModel):
     """
     Defines a class RegisteredVoters
     """
     __tablename__ = "REGISTERED_VOTERS"
-    reg_no = Column(Integer, primary_key=True)
+    reg_no = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True)
     First_Name = Column(String(256), nullable=False)
     Middle_Name = Column(String(256))
     Last_Name = Column(String(256))
@@ -31,33 +64,37 @@ class RegisteredVoters(Base):
     DOR = Column(DateTime(timezone=True), server_default=func.now())
 
 
-class Aspirants(Base):
+class Aspirants(Base, UserMixin, BaseModel):
     """
     Defines a class Aspirant's
     """
     __tablename__ = "ASPIRANTS"
-    asp_no = Column(Integer, primary_key=True)
-    id_no = Column(Integer, ForeignKey("REGISTERED_VOTERS.reg_no"))
-    pno = Column(Integer, ForeignKey("POST.pno"))
+    asp_no = Column(Integer, primary_key=True, autoincrement=True)
+    # fix this column below: id_no == id_no not reg_no
+    id_no = Column(Integer, ForeignKey("REGISTERED_VOTERS.id"), primary_key=True)
+    post_no = Column(Integer, ForeignKey("POST.post_no"), primary_key=True)
+    post_name = Column(String(256), nullable=False)
     First_Name = Column(String(256), nullable=False)
     Middle_Name = Column(String(256))
     Last_Name = Column(String(256))
+    photo = Column(String(256))
     Location = Column(String(256))
     Password = Column(LargeBinary)
     Email = Column(String(256))
     DOR = Column(DateTime(timezone=True), server_default=func.now())
+    no_of_votes = Column(Integer, default=0)
 
 
-class Post(Base):
+class Post(Base, BaseModel):
     """
     Defines a class Post
     """
     __tablename__ = "POST"
-    pno = Column(Integer, primary_key=True)
+    post_no = Column(Integer, primary_key=True, autoincrement=True)
     Post_Name = Column(String(256), nullable=False, unique=True)
 
 
-class Admin(Base):
+class Admin(Base, UserMixin, BaseModel):
     """
     Defines a class Admin
     """
@@ -72,41 +109,29 @@ class Admin(Base):
     DOR = Column(DateTime(timezone=True), server_default=func.now())
 
 
-class Votes(Base):
-    """
-    Defines a class Votes
-    """
-    __tablename__ = "VOTES"
-    vno = Column(Integer, primary_key=True)
-    asp_no = Column(Integer, ForeignKey("ASPIRANTS.asp_no"))
-    pno = Column(Integer, ForeignKey("POST.pno"))
-    seat_name = Column(String(256))
-    First_Name = Column(String(256), nullable=False)
-    Middle_Name = Column(String(256))
-    Last_Name = Column(String(256))
-    Location = Column(String(256))
-    Votes_guarnered = Column(Integer)
-    Percentage = Column(String(256))
-
-
-class Voters(Base):
+class Voters(Base, BaseModel):
     """
     Defines a class Voters that holds records
     for voters who have already logged into the
     system
     """
     __tablename__ = "VOTERS"
-    id = Column(Integer, primary_key=True)
-    reg_no = Column(Integer, ForeignKey("REGISTERED_VOTERS.reg_no"))
+    id_no = Column(Integer, ForeignKey("REGISTERED_VOTERS.id"), primary_key=True)
+    reg_no = Column(Integer, ForeignKey("REGISTERED_VOTERS.reg_no"), primary_key=True)
+    president = Column(Boolean, default=False)
+    senator = Column(Boolean, default=False)
+    governor = Column(Boolean, default=False)
+    mp = Column(Boolean, default=False)
     Status = Column(Enum(myEnum), default='NV')
     DOV = Column(DateTime(timezone=True), server_default=func.now())
 
-if __name__ == "__main__":
-    str1 = 'mysql://root:''@localhost:3306/VOTEAPP'  # Holds database info
-    engine = create_engine(str1)
-    #creates database first
-    #engine.connect().execute('CREATE DATABASE VOTEAPP;')
 
-    # Deletes Tables Before Creating them
+#creates database first
+# engine.connect().execute('CREATE DATABASE VOTEAPP;')
+
+# Deletes Tables Before Creating them
+if __name__ == "__main__":
+    """Create tables in the database"""
     Base.metadata.drop_all(engine)
+
     Base.metadata.create_all(engine)
