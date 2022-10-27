bd760d5431fa538d40d75c9c72b0deae8a4727b9
CodexRodney <rodney002mwanje@gmail.com>
2022-10-27 18:51:51 +0300

    Added Database credentials

--- Models/create_database.py
+++ Models/create_database.py
@@ -4,7 +4,8 @@ import sqlalchemy_utils as sq
 
 username = 'rod'
 password = 'r'
-str1 = f'mysql://{username}:{password}@localhost:3306/testdb'  # Holds database info
+database = 'VOTEAPP'
+str1 = f'mysql://{username}:{password}@localhost:3306/{database}'  # Holds database info
 engine = create_engine(str1)
 if not sq.database_exists(engine.url):
     sq.create_database(engine.url)
