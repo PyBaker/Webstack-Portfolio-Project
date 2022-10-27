2164b046795ac25163009e445e4d6c6cd58a7c20
CodexRodney <rodney002mwanje@gmail.com>
2022-10-28 01:39:41 +0300

    Testing queries

--- Models/test.py
+++ Models/test.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 import sqlalchemy_utils as sq
+from sqlalchemy.orm import sessionmaker
+from tables import Aspirants
 
 
 username = "rod"
 password = "r"
 str1 = f'mysql://{username}:{password}@localhost:3306/VOTEAPP'  # Holds database info
 engine = create_engine(str1)
-if not sq.database_exists(engine.url):
-    sq.create_database(engine.url)
+DbSession = sessionmaker(bind=engine)
+session = DbSession()
+aspirants = session.query(Aspirants.First_Name, Aspirants.Middle_Name).filter(Aspirants.post_name == "President").all()
 
-print(sq.database_exists(engine.url))
+print(aspirants)
